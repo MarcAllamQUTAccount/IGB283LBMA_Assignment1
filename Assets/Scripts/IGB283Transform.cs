ec06bed62cdfefe427b1047a7a31670baac228b5
MarcAllamQUTAccount <n12357146@qut.edu.au>
2026-09-02 13:46:01 +1000

    ybun

--- Assets/Scripts/IGB283Transform.cs
+++ Assets/Scripts/IGB283Transform.cs
@@ -3,35 +3,15 @@ using UnityEngine;
 public static class IGB283Transform
 {
     // Translation matrix
-    public static Matrix3x3 Translation(IGB283Vector t)
+    public static Matrix3x3 Translation(IGB283Vector pos)
     {
-        Matrix3x3 m = Matrix3x3.identity;
-
-        // 2D homogeneous:
-        // [1 0 tx]
-        // [0 1 ty]
-        // [0 0 1 ]
-        m[0, 2] = t.x;
-        m[1, 2] = t.y;
-
-        return m;
+        return new Matrix3x3(
+            new IGB283Vector(1, 0, pos.x),
+            new IGB283Vector(0, 1, pos.y),
+            new IGB283Vector(0, 0, 1)
+            );
     }
 
-    // Scaling matrix (uniform or non-uniform)
-    public static Matrix3x3 Scaling(IGB283Vector s)
-    {
-        Matrix3x3 m = Matrix3x3.identity;
-
-        // [sx 0  0]
-        // [0  sy 0]
-        // [0  0  1]
-        m[0, 0] = s.x;
-        m[1, 1] = s.y;
-
-        return m;
-    }
-
-    // Rotation around Z axis (2D rotation)
     public static Matrix3x3 Rotation(float angleRadians)
     {
         float c = Mathf.Cos(angleRadians);
@@ -49,24 +29,25 @@ public static class IGB283Transform
         return m;
     }
 
-    // Build TRS matrix: T * R * S
+    public static Matrix3x3 Scale(IGB283Vector s)
+    {
+        return new Matrix3x3(
+            new IGB283Vector(s.x, 0, 0),
+            new IGB283Vector(0, s.y, 0),
+            new IGB283Vector(0, 0, 1)
+        );
+    }
     public static Matrix3x3 TRS(IGB283Vector pos, float angleRadians, IGB283Vector scale)
     {
         Matrix3x3 T = Translation(pos);
         Matrix3x3 R = Rotation(angleRadians);
-        Matrix3x3 S = Scaling(scale);
+        Matrix3x3 S = Scale(scale);
 
-        // Order: T * R * S
         return T * R * S;
     }
 
-    // Apply matrix to our custom vector
     public static IGB283Vector Apply(Matrix3x3 m, IGB283Vector v)
     {
-        // Convert to Unity Vector3 with z=1 for homogeneous coordinates
-        Vector3 p = new Vector3(v.x, v.y, 1f);
-        Vector3 result = m.MultiplyPoint(p); // uses your Matrix3x3
-
-        return new IGB283Vector(result.x, result.y, v.z);
+        return m.MultiplyPoint(v);
     }
 }
